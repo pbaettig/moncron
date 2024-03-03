52e86e752cfe9534f7ac5c67c36497304e9694f8
Pascal Bättig <pbaettig@gmail.com>
2024-03-03 15:30:14 +0100

    fix typo in server cmdline arg

--- cmd/server/args.go
+++ cmd/server/args.go
@@ -16,7 +16,7 @@ func (c *cmdlineArgs) Parse() error {
 	flag.StringVar(&c.listenAddress, "listen", "0.0.0.0", "Listen address")
 	flag.IntVar(&c.listenPort, "port", 8088, "Listen port")
 	flag.StringVar(&c.dbPath, "db", "test.db", "Path to sqlite DB file")
-	flag.DurationVar(&c.shutdownTimeout, "gtimeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.DurationVar(&c.shutdownTimeout, "timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
 	flag.Parse()
 
 	return nil
